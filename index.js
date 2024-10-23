0b788bf797e55d58542c98ad9745f78adbb5c4e5
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-23 15:58:13 -0300

    created get by id

--- index.js
+++ index.js
@@ -18,14 +18,37 @@ var DB = {
             title: "Counter Strike",
             year: 2010,
             price: 25
+        },
+        {
+            id:61,
+            title: "Lego Star Wars",
+            year: 2022,
+            price: 190
         }
     ]
 }
 
-app.get("/",()=>{
-
+app.get("/games",(req,res)=>{
+    res.statusCode = 200;
+    res.json(DB.games);
 });
 
+app.get("/game/:id",(req,res)=>{
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    } else{
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+            res.json(game);
+            res.statusCode = 200;
+        } else{
+            res.sendStatus(404);
+        }
+    }
+});
 
 
 app.listen(45678,()=>{
