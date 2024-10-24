e9c44db17c39034d1f5598a5fc910fd10677e180
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-24 15:34:53 -0300

    added HATEOAS

--- APIGAMES/index.js
+++ APIGAMES/index.js
@@ -25,7 +25,7 @@ function auth(req,res,next){
                 res.status(401);
                 res.json({err: "invalid token"});
             } else{
-                
+
                 req.token = token;
                 req.loggedUser = {id: data.id, email: data.email};
                 next();
@@ -76,8 +76,32 @@ var DB = {
 }
 
 app.get("/games",auth,(req,res)=>{
+
+    var HATEOAS = [
+        {
+            href: "http://localhost:45678/game/0",
+            method: "GET",
+            rel: "get_game"
+        },
+        {
+            href: "http://localhost:45678/auth",
+            method: "POST",
+            rel: "login"
+        },
+        {
+            href: "http://localhost:45678/game/0",
+            method: "PUT",
+            rel: "edit_game"
+        },
+        {
+            href: "http://localhost:45678/game/0",
+            method: "DELETE",
+            rel: "delete_game"
+        }
+    ]
+
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({games: DB.games, _links: HATEOAS});
 });
 
 app.get("/game/:id",auth,(req,res)=>{
@@ -87,10 +111,34 @@ app.get("/game/:id",auth,(req,res)=>{
     } else{
 
         var id = parseInt(req.params.id);
+
+        var HATEOAS = [
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "GET",
+                rel: "get_game"
+            },
+            {
+                href: "http://localhost:45678/games/",
+                method: "GET",
+                rel: "get_all_games"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "PUT",
+                rel: "edit_game"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "DELETE",
+                rel: "delete_game"
+            }
+        ]
+
         var game = DB.games.find(g => g.id == id);
 
         if(game != undefined){
-            res.json(game);
+            res.json({game, _links: HATEOAS});
             res.statusCode = 200;
         } else{
             res.sendStatus(404);
